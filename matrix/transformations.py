312604f744cc520d6aac8dcf0d039cc3d6ee4358
Timothy Ong <tim.ong.118@gmail.com>
2014-03-20 20:41:50 -0400

    sphere doesnt work

--- matrix/transformations.py
+++ matrix/transformations.py
@@ -51,35 +51,65 @@ def parse():
             edge_mat = mult(trans_mat, edge_mat)
         elif parts[0] == "render-parallel":
             render_parallel()
+        elif parts[0] == "sphere":
+            sphere(float(parts[1]),float(parts[2]),float(parts[3]),float(parts[4]))
+        elif parts[0] == "render-perspective-cyclops":
+            render_perspective_cyclops(float(parts[1]),float(parts[2]),float(parts[3]),"255 255 255 ")
+        elif parts[0] == "render-perspective-stereo":
+            new_edge = []
+            for x in range(len(edge_mat)):
+                new_edge.append(edge_mat[x])
+            ex = float(parts[1])
+            ey = float(parts[2])
+            ez = float(parts[3])
+            ax = float(parts[4])
+            ay = float(parts[5])
+            az = float(parts[6])
+            new_mat = new_edge
+            for x in range(len(new_mat) / 2):
+                draw_line("255 0 0 ", 
+                          ((ex-(ez * (new_edge[2*x][0] - ex) / (new_edge[2*x][2] - ez))) + wmove_pix) * wscale,
+                          ((ey-(ez * (new_edge[2*x][1] - ey) / (new_edge[2*x][2] - ez))) + hmove_pix) * hscale,
+                          ((ex-(ez * (new_edge[2*x+1][0] - ex) / (new_edge[2*x+1][2] - ez))) + wmove_pix) * wscale,
+                          ((ey-(ez * (new_edge[2*x+1][1] - ey) / (new_edge[2*x+1][2] - ez))) + hmove_pix) * hscale)
+                draw_line("0 180 180 ",
+                          ((ax-(az * (new_edge[2*x][0] - ax) / (new_edge[2*x][2] - az))) + wmove_pix) * wscale,
+                          ((ay-(az * (new_edge[2*x][1] - ay) / (new_edge[2*x][2] - az))) + hmove_pix) * hscale,
+                          ((ax-(az * (new_edge[2*x+1][0] - ax) / (new_edge[2*x+1][2] - az))) + wmove_pix) * wscale,
+                          ((ay-(az * (new_edge[2*x+1][1] - ay) / (new_edge[2*x+1][2] - az))) + hmove_pix) * hscale)
         elif parts[0] == "clear-edges":
             clear_edge()
         elif parts[0] == "clear-pixels":
             clear_pix()
         elif parts[0] == "file":
-            output = open(parts[1], "w")
-            output.write("P3 500 500 255 ")
-            for x in pic:
-                for y in x:
-                    output.write(y)
-            output.close()
+            file(parts[1])
         elif parts[0] == "end":
             f.close()
             break
 
-def draw_line(x1,y1,x2,y2):
+def file(name):
+    global max_height, max_width
+    output = open(name, "w")
+    output.write("P3 %s %s 255 "%(max_width, max_height))
+    for x in pic:
+        for y in x:
+            output.write(y)
+    output.close()
+
+def draw_line(color,x1,y1,x2,y2):
     x1 = int(x1)
     x2 = int(x2)
     y1 = int(y1)
     y2 = int(y2)
     if x1 == x2 and y1 == y2:
-        pic[y1][x1] = "255 255 255 "
+        pic[y1][x1] = color
     elif x1 == x2:
         if y1 > y2:
             x1,y1,x2,y2 = x2,y2,x1,y1
         x = x1
         y = y1
         while y <= y2:
-            pic[y][x] = "255 255 255 "
+            pic[y][x] = color
             y += 1
     elif y1 == y2:
         if x1 > x2:
@@ -87,7 +117,7 @@ def draw_line(x1,y1,x2,y2):
         x = x1
         y = y1
         while x <= x2:
-            pic[y][x] = "255 255 255 "
+            pic[y][x] = color
             x += 1
     elif math.fabs(x1-x2) >= math.fabs(y1-y2):
         #x-major   
@@ -104,7 +134,7 @@ def draw_line(x1,y1,x2,y2):
                 if acc >= x2 - x1:
                     y += 1
                     acc -= (x2 - x1)
-                pic[y][x] = "255 255 255 "
+                pic[y][x] = color
             #end up                  
         else:
             #down    
@@ -117,7 +147,7 @@ def draw_line(x1,y1,x2,y2):
                 if acc >= x2 - x1:
                     y -= 1
                     acc -= (x2 - x1)
-                pic[y][x] = "255 255 255 "
+                pic[y][x] = color
             #end down
         #end x-major
     else:
@@ -133,7 +163,7 @@ def draw_line(x1,y1,x2,y2):
                 if acc >= y2 - y1:
                     x += 1
                     acc -= (y2 - y1)
-                pic[y][x] = "255 255 255 "
+                pic[y][x] = color
         else:
             x = x1
             y = y1
@@ -144,26 +174,32 @@ def draw_line(x1,y1,x2,y2):
                 if acc >= y2 - y1:
                     x -= 1
                     acc -= (y2 - y1)
-                pic[y][x] = "255 255 255 "
+                pic[y][x] = color
 
 def render_parallel():
     new_mat = edge_mat
-    print new_mat
-    print edge_mat
     for x in range(len(edge_mat)):
         new_mat[x][0] = (edge_mat[x][0] + wmove_pix) * wscale
         new_mat[x][1] = (edge_mat[x][1] + hmove_pix) * hscale
-    print new_mat
-    print wmove_pix, hmove_pix, wscale, hscale
     for x in range(len(new_mat) / 2):
-        draw_line(new_mat[2*x][0], new_mat[2*x][1], new_mat[2*x+1][0], new_mat[2*x+1][1])
+        draw_line("255 255 255 ", new_mat[2*x][0], new_mat[2*x][1], new_mat[2*x+1][0], new_mat[2*x+1][1])
+
+def render_perspective_cyclops(ex,ey,ez,color):
+    new_mat = edge_mat
+    for x in range(len(edge_mat)):
+        new_mat[x][0] = ((ex-(ez * (edge_mat[x][0] - ex) / (edge_mat[x][2] - ez))) + wmove_pix) * wscale
+        new_mat[x][1] = ((ey-(ez * (edge_mat[x][1] - ey) / (edge_mat[x][2] - ez))) + hmove_pix) * hscale
+    print new_mat
+    for x in range(len(new_mat) / 2):
+        draw_line(color, new_mat[2*x][0], new_mat[2*x][1], new_mat[2*x+1][0], new_mat[2*x+1][1])
 
 def world_to_render(xleft, ybottom, xright, ytop, width, height):
     global pic
     global wmove_pix, hmove_pix, wscale, hscale
-    pic = [["0 0 0 " for y in range(height)] for x in range(width)]
+    global max_width, max_height
     max_width = width
     max_height= height
+    pic = [["0 0 0 " for y in range(width)] for x in range(height)]
     wmove_pix = 0 - xleft
     hmove_pix = 0 - ybottom
     wscale = int(width / (xright - xleft))
@@ -177,13 +213,34 @@ def clear_pic():
     global pic
     pic = [["0 0 0 " for y in range(max_height)] for x in range(max_width)]
 
+def sphere(r,cx,cy,cz):
+    semi = []
+    long_points = []
+    for x in range(18):
+        semi.append([cx + r * math.cos(math.radians(x * 10)),
+                     cy + r * math.sin(math.radians(x * 10)),
+                     cz,1])
+    for x in range(36):
+        long_points.append(semi)
+        semi = mult([[1,0,0,0],
+                     [0,0-math.cos(math.radians(10*x)),0-math.sin(math.radians(10*x)),0],
+                     [math.sin(math.radians(10*x)),0-math.cos(math.radians(10*x)),0, 0],
+                     [0,0,0,1]], semi)
+    for x in range(36):
+        for y in range(18):
+            add_line(long_points[x][y][0],long_points[x][y][1],long_points[x][y][2],
+                     long_points[x][(y+1)%18][0],long_points[x][(y+1)%18][1],long_points[x][(y+1)%18][2])
+    for x in range(18):
+        for y in range(36):
+            add_line(long_points[y][x][0],long_points[y][x][1],long_points[y][x][2],
+                     long_points[(y+1)%36][x][0],long_points[(y+1)%36][x][1],long_points[(y+1)%36][x][2])
+
 def add_line(x1,y1,z1,x2,y2,z2):
     global edge_mat
     length = len(edge_mat)
-    points = [[x1,0 - y1,z1,1],[x2,0 - y2,z2,1]]
+    points = [[0-x1,0-y1,z1,1],[0-x2,0-y2,z2,1]]
     for x in range(2):
         edge_mat.append(points[x])
-    print edge_mat
 
 def make_iden(mat):
     for x in range(4):
@@ -198,7 +255,7 @@ def transform(x1,y1,z1):
     new_mat = make_iden(new_mat)
     values = [0 - x1,0 - y1,z1,1]
     for x in range(4):
-        new_mat[x][3] = values[x]
+        new_mat[3][x] = values[x]
     trans_mat = mult(new_mat,trans_mat)
 
 def scale(x1,y1,z1):
@@ -214,30 +271,30 @@ def rotate_x(rx):
     global trans_mat
     new_mat = [ [ 0 for x in range(4) ] for y in range(4) ]
     new_mat = make_iden(new_mat)
-    new_mat[1][1] = math.cos(math.radians(rx))
-    new_mat[1][2] = 0 - math.sin(math.radians(rx))
-    new_mat[2][1] = math.sin(math.radians(rx))
-    new_mat[2][2] = math.cos(math.radians(rx))
+    new_mat[1][1] = math.cos(math.radians(0-rx))
+    new_mat[1][2] = 0 - math.sin(math.radians(0-rx))
+    new_mat[2][1] = math.sin(math.radians(0-rx))
+    new_mat[2][2] = math.cos(math.radians(0-rx))
     trans_mat = mult(new_mat,trans_mat)
 
 def rotate_y(ry):
     global trans_mat    
     new_mat = [ [ 0 for x in range(4) ] for y in range(4) ]
     new_mat = make_iden(new_mat)
-    new_mat[0][0] = math.cos(math.radians(ry))
-    new_mat[0][2] = math.sin(math.radians(ry))
-    new_mat[2][0] = 0 - math.sin(math.radians(ry))
-    new_mat[2][2] = math.cos(math.radians(ry))
+    new_mat[0][0] = math.cos(math.radians(0-ry))
+    new_mat[0][2] = math.sin(math.radians(0-ry))
+    new_mat[2][0] = 0 - math.sin(math.radians(0-ry))
+    new_mat[2][2] = math.cos(math.radians(0-ry))
     trans_mat = mult(new_mat,trans_mat)
 
 def rotate_z(rz):
     global trans_mat
     new_mat = [ [ 0 for x in range(4) ] for y in range(4) ]
     new_mat = make_iden(new_mat)
-    new_mat[0][0] = math.cos(math.radians(rz))
-    new_mat[0][1] = 0 - math.sin(math.radians(rz))
-    new_mat[1][0] = math.sin(math.radians(rz))
-    new_mat[1][1] = math.cos(math.radians(rz))
+    new_mat[0][0] = 0-math.cos(math.radians(rz))
+    new_mat[0][1] = 0 - math.sin(math.radians(0-rz))
+    new_mat[1][0] = math.sin(math.radians(0-rz))
+    new_mat[1][1] = 0-math.cos(math.radians(rz))
     trans_mat = mult(new_mat,trans_mat)
 
 def mult(mat,mat2):
@@ -251,3 +308,4 @@ def mult(mat,mat2):
     return new_mat
 
 parse()
+sphere(5,1,1,1)
